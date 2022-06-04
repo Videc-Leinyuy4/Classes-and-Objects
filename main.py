682358fb676afb066c13e8d72b52a1278c387740
Videc-Leinyuy4 <30395297+Videc-Leinyuy4@users.noreply.github.com>
2022-06-04 03:08:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
-
+    def __init__(self, name, age, tracks, score):
+       self.name = name
+       self.age = age
+       self.tracks = tracks
+       self.score = score
+    
+    def change_name(self,new_name): {new_name}"):
+        self.name = new_name
+        print(f"Changed name to {new_name}")
+    
+    def change_age(self, new_age):
+        try:
+            new_age = int(new_age)
+        except:
+            print ("Failed to change age. Invalid data type")
+            raise Exception
+            
+        self.age = new_age
+        print(f"Changed age to {new_age}")
+     
+    def add_track (self, new_track)
+        self.tracks
+        self.tracks.append(new_track)
+        print(f"Added new track: {new_track}")
+        
+    def get_score(self):
+        return self.score  
+                
 
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
